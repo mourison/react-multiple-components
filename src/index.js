1d654a92e08cd4e68c55760478bfeddf6bf82471
mourison <georgetsamtsouris@gmail.com>
2021-02-14 17:42:27 +0200

    Uses props children attribute example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
       <div className="ui container comments">
-        <CommentDetail author="George" timeAgo="Today at 5:00PM" content="Nice blog from George" avatar ="https://source.unsplash.com/random"/>
-        <CommentDetail author="Nikos" timeAgo="Today at 5:00PM" content="Nice blog from George" avatar ="https://source.unsplash.com/random"/>
+        <ApprovalCard>
+          <CommentDetail author="George" timeAgo="Today at 5:00PM" content="Nice blog from George" avatar ="https://source.unsplash.com/random"/>
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail author="Nikos" timeAgo="Today at 5:00PM" content="Nice blog from George" avatar ="https://source.unsplash.com/random"/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+                Are you sure that you want to do this?
+            </div>
+        </ApprovalCard>
       </div>
   );
 };
