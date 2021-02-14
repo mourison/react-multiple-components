b938154e26876ca1590e27c9411bb2b3ef440875
mourison <georgetsamtsouris@gmail.com>
2021-02-14 12:16:17 +0200

    Creates nested component

---
+++ src/CommentDetail.js
@@ -0,0 +1,22 @@
+import react from 'react';
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src='https://source.unsplash.com/random'/>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog Post</div>
+      </div>
+  </div>
+  );
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
       <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-            <img alt="avatar" src='https://source.unsplash.com/random'/>
-          </a>
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog Post</div>
-          </div>
-        </div>
+        <CommentDetail/>
       </div>
   );
 };
